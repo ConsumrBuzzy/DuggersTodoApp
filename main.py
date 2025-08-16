d181c9fb9e472072e56f2b5f0622e4cae3f3dfad
ConsumrBuzzy <robert.d@consumrbuzz.com>
2025-08-16 19:25:43 -0400

    refactor: extract TodoList class from main.py and implement list management functionality

--- main.py
+++ main.py
@@ -1,40 +1,21 @@
 from datetime import datetime
 from todoItem import TodoItem
+from todoList import TodoList
 
 # Main Function
 def main():
-    todo_list = []
+    todoList = TodoList()
+
     print("Hello World")
     while True:
         user_input = input("Enter 'new' to add a new task, 'q' to quit, 'print' to print the list): ")
         if user_input.lower() == 'q' or user_input.lower() == 'quit':
             break
         elif user_input.lower() == 'print' or user_input.lower() == 'p':
-            for todo in todo_list:
-                print(str(todo_list.index(todo)+1) + ": " + todo.taskName )
-                user_input = input("Enter the number corresponding to a task to select it: ")
-                if user_input.isdigit():
-                    selectedTodo = todo_list[int(user_input)-1]
-                    user_input = input("Enter 'edit' to edit the task, 'complete' to complete the task, 'start' to start the task, 'quit' to quit, 'print' to print the task, 'delete' to delete the task: ")
-                    if user_input.lower() == 'edit' or user_input.lower() == 'e':
-                        selectedTodo.editTask()
-                    elif user_input.lower() == 'complete' or user_input.lower() == 'c':
-                        selectedTodo.completeTask()
-                    elif user_input.lower() == 'start' or user_input.lower() == 's':
-                        selectedTodo.startTask()
-                    elif user_input.lower() == 'quit' or user_input.lower() == 'q':
-                        break
-                    elif user_input.lower() == 'print' or user_input.lower() == 'p':
-                        print(selectedTodo)
-                    elif user_input.lower() == 'delete' or user_input.lower() == 'd':
-                        todo_list.remove(selectedTodo)
-                    else:
-                        print("Invalid input")
-                else:
-                    print("Invalid input")
+            todoList.printTodoList()
         elif user_input.lower() == 'new' or user_input.lower() == 'n':
-            todo_list.append(TodoItem(False, 0, 0, 0, "", "", 0))
-            print(todo_list[-1])
+            todoList.addTodo(TodoItem())
+            print(todoList.todoList[-1])
         else:
             print("Invalid input")
 
---
+++ todoList.py
@@ -0,0 +1,44 @@
+from todoItem import TodoItem
+
+class TodoList:
+    # Todo List
+    todoList = []
+    # Constructor
+    def __init__(self):
+        self.todoList = []
+    # Add Todo
+    def addTodo(self, todoItem):
+        self.todoList.append(todoItem)
+    # Remove Todo
+    def remove(self, todoItem):
+        if todoItem in self.todoList:
+            for todo in self.todoList:
+                if todo == todoItem:
+                    self.todoList.remove(todoItem)
+
+    # Print Todo List
+    def printTodoList(self):
+        for todo in self.todoList:
+            print(str(self.todoList.index(todo)+1) + ": " + todo.taskName)
+        user_input = input("Enter the number corresponding to a task to select it: ")
+        if user_input.isdigit():
+            selectedTodo = self.todoList[int(user_input)-1]
+            user_input = input("Enter 'edit' to edit the task, 'complete' to complete the task, 'start' to start the task, 'quit' to quit, 'print' to print the task, 'delete' to delete the task: ")
+            if user_input.lower() == 'edit' or user_input.lower() == 'e':
+                selectedTodo.editTask()
+            elif user_input.lower() == 'complete' or user_input.lower() == 'c':
+                selectedTodo.completeTask()
+            elif user_input.lower() == 'start' or user_input.lower() == 's':
+                selectedTodo.startTask()
+            elif user_input.lower() == 'quit' or user_input.lower() == 'q':
+                return
+            elif user_input.lower() == 'print' or user_input.lower() == 'p':
+                print(selectedTodo)
+            elif user_input.lower() == 'delete' or user_input.lower() == 'd':
+                self.remove(selectedTodo)
+        else:
+            print("Invalid input")
+
+    # String Representation
+    def __str__(self):
+        return "\n".join([str(todo) for todo in self.todoList])
