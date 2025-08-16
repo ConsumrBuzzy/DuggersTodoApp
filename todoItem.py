ca1385d4224a1f228f1513150e2543ab5e0fe360
ConsumrBuzzy <robert.d@consumrbuzz.com>
2025-08-16 17:44:28 -0400

    refactor: move TodoItem class to separate module for better code organization

---
+++ todoItem.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+
+class TodoItem:
+    # Task Name
+    taskName = ""
+    # Task Description
+    taskDescription = ""
+    # Task Category
+    taskCategory = ""
+    # Task Priority
+    taskPriority = 0
+    # Task Difficulty
+    taskDifficulty = 0
+    # Task Duration
+    taskDuration = 0
+    # Task Completion Status
+    isCompleted = False
+    # Task Completion Time
+    completedAt = None
+    # Task Creation Time
+    createdAt = None
+    # Task Estimated Duration
+    estimatedDuration = 0
+    # Task Start Time
+    startedAt = None
+    
+    # Constructor
+    def __init__(self, taskName, isCompleted=False, taskPriority=0, taskDifficulty=0, taskDuration=0, taskCategory="", taskDescription="", estimatedDuration=0):
+        self.taskName = taskName
+        self.isCompleted = isCompleted
+        self.createdAt = datetime.now()
+        self.completedAt = None
+        self.startedAt = None
+        self.estimatedDuration = estimatedDuration
+        self.taskDuration = taskDuration
+        self.taskPriority = taskPriority
+        self.taskDifficulty = taskDifficulty
+        self.taskCategory = taskCategory
+        self.taskDescription = taskDescription
+
+    def __str__(self):
+        return f"Task Name: {self.taskName}\nTask Priority: {self.taskPriority}\nTask Difficulty: {self.taskDifficulty}\nTask Category: {self.taskCategory}\nTask Description: {self.taskDescription}\nTask Duration: {self.taskDuration}\nTask Completion Status: {self.isCompleted}\nTask Completion Time: {self.completedAt}\nTask Creation Time: {self.createdAt}\nTask Estimated Duration: {self.estimatedDuration}\nTask Start Time: {self.startedAt}"
