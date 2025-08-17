32a7dd2103830b193ecf80b97991b2c45c871789
ConsumrBuzzy <robert.d@consumrbuzz.com>
2025-08-17 17:38:01 -0400

    refactor: change print command to list for better UX consistency

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ def main():
 
     print("Hello World")
     while True:
-        user_input = input("Enter 'new' to add a new task, 'q' to quit, 'print' to print the list, 'save' to save the list, 'load' to load the list: ")
+        user_input = input("Enter 'new' to add a new task, 'q' to quit, 'list' to print the list, 'save' to save the list, 'load' to load the list: ")
         if user_input.lower() == 'q' or user_input.lower() == 'quit':
             break
-        elif user_input.lower() == 'print' or user_input.lower() == 'p':
+        elif user_input.lower() == 'list' or user_input.lower() == 'l':
             todoList.printTodoList()
         elif user_input.lower() == 'new' or user_input.lower() == 'n':
             task_name = input("Enter a task name: ")
